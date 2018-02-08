8a28e7f399dba2e24c8438d6029dcdf7d4a081a0
Daniel Novotny <svc_okc_bt204@ad.osuokc.edu>
2018-02-07 18:25:55 -0600

    FOOBAR

---
+++ CIS2013_Week4_Quiz1/FooBar.cpp
@@ -0,0 +1,26 @@
+/* print 1-2000 if /50 print foo, if /33 print bar*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 0;
+	int b = 0;
+	for (int i = 1; i < 2000; i++)
+	{
+		cout << i;
+		if (i%50 == 0) {
+			cout << " FOO";
+		}
+
+		if (i % 33 == 0) {
+			cout << " BAR";
+		}
+
+				
+		cout << endl;
+	
+		
+	}
+	cin >> a;
+}
